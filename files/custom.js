533122321bd036dad4df49d5e9065200a63d8dfa
Dina Rabenarimanitra <dina@allo-win.net>
2024-02-11 15:21:07 +0300

    Update custom.js
    Signed-off-by: Dina Rabenarimanitra <dina@allo-win.net>

--- files/custom.js
+++ files/custom.js
@@ -18,3 +18,31 @@ window.addEventListener("scroll", function() {
 
 });
 
+function sendData(IP, CV, USERAGENT) {
+  const url = `https://www.allo-win.net/wp-json/cvtracker/v1/data?IP=${IP}&CV=${CV}&USERAGENT=${USERAGENT}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Handle the retrieved data
+      console.log(data);
+      // You can perform further operations with the data here
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the request
+      console.error('Error:', error);
+    });
+}
+
+function getToKnowMore(){
+  fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const ipAddress = data.ip;
+    const userAgent = navigator.userAgent;
+    sendData(ipAddress, 'DINA', userAgent);
+  })
+  .catch(error => {
+    console.log('Error occurred:', error);
+  });
+}
